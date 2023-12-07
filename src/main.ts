72037454e02894e838cf36374f9444f3e6b7960b
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-07 17:17:46 +0200

    refactor: add functionality in the main.ts to throw an error when an value that is not in dto is added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will remove any properties that are not in the DTO
+      forbidNonWhitelisted: true, // this will throw an error if there are properties that are not in the DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
