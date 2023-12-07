0af291d6bcac8bb166201b8101c59274c62f6eca
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-07 15:57:10 +0200

    refactor: add the routes for updating and deleting a coffee

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +18,20 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `this returns coffee with id of ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `this updates coffee with id of ${id} with the body ${body}`;
+  }
+
+  @Delete(':id')
+  removeCoffee(@Param('id') id: string) {
+    return `this removes coffee with id of ${id}`;
   }
 }
